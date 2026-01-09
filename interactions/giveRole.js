30db008bde30e044e924e58633b652062c37f2d8
Luca <luca.kloger@gmail.com>
2026-01-09 22:29:29 +0100

    Remove Role added

--- interactions/giveRole.js
+++ interactions/giveRole.js
@@ -2,7 +2,7 @@ export async function giveRole(interation) {
   const member = interation.member;
 
   await interation.deferReply({ ephemeral: true });
-  
+
   if (member.user.username != "krabben_luc") {
     console.log("Unauthorized user attempted to use giveRole.");
     await interation.editReply({ content: "Canceled!", ephemeral: true });
@@ -11,6 +11,15 @@ export async function giveRole(interation) {
 
   const roleName = interation.options.getString("99999");
   const role = member.guild.roles.cache.find((role) => role.name === roleName);
+
+  if (member.roles.cache.find((r) => r.name === roleName)) {
+    await interation.editReply({
+      content: "Removed!",
+      ephemeral: true,
+    });
+    await member.roles.remove(role);
+    return;
+  }
 
   if (role) {
     try {
